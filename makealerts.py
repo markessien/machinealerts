48065323b379aee459254f56f020a49e6b408858
Deji Joseph <wrecodde@gmail.com>
2018-11-09 22:49:52 +0100

    added alert generator helper functions
    randomised alert statements can be generated with the helper functions

--- makealerts.py
+++ makealerts.py
@@ -15,7 +15,7 @@ dataset_size = len(data)
 time_stamps = []
 
 for _ in range(dataset_size):
-	# assume working hours too to be 7am to 7pm
+	# assume working hours: 7am to 7pm
 	hour = random.choice(range(7,20))
 	min = random.choice(range(60))
 	time_stamp = "{:0>2s}:{:0>2s}".format(str(hour), str(min))
@@ -33,12 +33,49 @@ dataset = [row for row in zip(prices, time_stamps)]
 	# late payment: 1
 	# split payment (assume 2 installments): 2
 	
-	# normal (1 min after purchase): 3
+	# normal (1 min before sale): 3
 
 # intent is to randomise alerts
 cases = [0, 1, 2, 3]
 
 def early_pmt(tm_stamp, price):
 	# 5 to 30 mins early
-	diff = random.choice(range(5, 30))
+	diff_in_mins = random.choice(range(5, 30))
+	diff_in_secs = diff_in_mins * 60
 	
+	hr, min = tm_stamp.split(":")
+	sales_time = datetime(2018, 11, 12, int(hr), int(min))
+	
+	alert_time = sales_time - timedelta(seconds=diff_in_secs)
+	return alert_time, price
+
+def late_pmt(tm_stamp, price):
+	# 5 to 30 mins late
+	diff_in_mins = random.choice(range(5, 30))
+	diff_in_secs = diff_in_mins * 60
+	
+	hr, min = tm_stamp.split(":")
+	sales_time = datetime(2018, 11, 12, int(hr), int(min))
+	
+	alert_time = sales_time + timedelta(seconds=diff_in_secs)
+	return alert_time, price
+
+def normal_pmt(tm_stamp, price):
+	# a minute early
+	diff_in_secs = 60
+	
+	hr, min = tm_stamp.split(":")
+	sales_time = datetime(2018, 11, 12, int(hr), int(min))
+	
+	alert_time = sales_time - timedelta(seconds=diff_in_secs)
+	return alert_time, price
+
+def split_pmt(tm_stamp, price):
+	# a payment before and another after
+	tm_before = early_pmt(tm_stamp, price)[0]
+	tm_after = late_pmt(tm_stamp, price)[0]
+	
+	pr_before = int(price) * 0.75
+	pr_after = int(price) * 0.25
+	
+	return [tm_before, pr_before], [tm_after, tm_after]
